b2f3ead51c6d4e7ef3613fd8b7eaa0e2920e9704
Ryan Mendez <rjmendez80@gmail.com>
2020-09-05 13:12:22 -0700

    prompt user for gitHub user name

---
+++ app.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'gitHubUserName',
+        message: 'Please enter you gitHub username: ',
+    },
+    {
+        type:
+    }
+
+])
